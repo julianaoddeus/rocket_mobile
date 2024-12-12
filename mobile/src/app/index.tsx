5cdbc925cab0594251c44dc82286dfe405eef202
julianaoddeus <julianaacademi@gmail.com>
2024-12-11 21:06:07 -0300

    finalizando botão na página1 e inserindo rota, inserindo API ao projeto

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
+import { router}  from "expo-router"
 
+import { Button } from "@/components/button";
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+
 
 export default function Index() {
   return (
@@ -14,6 +17,10 @@ export default function Index() {
     >
       <Welcome />
       <Steps />
+
+      <Button onPress={() => router.navigate("/home")}>
+       <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
